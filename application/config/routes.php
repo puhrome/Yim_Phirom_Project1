668dc530406cd0d87a030d49700bfe16f938fc8a
puhrome <phirom.yim@fullsail.edu>
2015-11-03 08:43:15 -0500

    Added Shopping Controller Class
    Rerouting index.php to ‘shopping’ class

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+//$route['default_controller'] = 'welcome';  // change it this to the name of your class
+$route['default_controller'] = 'shopping'; //change to 'shopping' controller class
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/controllers/shopping.php
@@ -0,0 +1,9 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Shopping extends CI_Controller
+{
+    public function index(){
+        echo '<h2>This is my shopping page</h2>';
+    }
+}
