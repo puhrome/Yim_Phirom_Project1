a5be92a96baef9d6cb092bdabbd3dd9e67c5d9d7
puhrome <phirom.yim@fullsail.edu>
2015-11-04 11:55:49 -0500

    Updated autoloader and config
    Added database and email in autoload

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -14,10 +14,10 @@
     <input type="text" name="username" value="" size="50" />
 
     <h5>Password</h5>
-    <input type="text" name="password" value="" size="50" />
+    <input type="password" name="password" value="" size="50" />
 
     <h5>Password Confirm</h5>
-    <input type="text" name="passconf" value="" size="50" />
+    <input type="password" name="passconf" value="" size="50" />
 
     <h5>Email Address</h5>
     <input type="text" name="email" value="" size="50" />
--- application/views/templates/navigation.php
+++ application/views/templates/navigation.php
@@ -19,12 +19,7 @@
                     <li><a href="#">About</a></li>
                     <li><a href="#">Explore</a></li>
                 </ul>
-                <form class="navbar-form navbar-right" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
+
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sign In <span class="caret"></span></a>
@@ -35,6 +30,12 @@
                         </ul>
                     </li>
                 </ul>
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
