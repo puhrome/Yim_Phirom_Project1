83f27eb156e7053ee6b5c4320480a3a92fe869d5
puhrome <phirom.yim@fullsail.edu>
2015-11-04 08:17:44 -0500

    Added form.php

---
+++ application/views/newsletter_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Newsletter</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    
+</head>
+<body>
+
+<div></div>
+</body>
+</html>
