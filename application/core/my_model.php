557d0d41d7a7acb2b086595e681ccfcc2bc73aa2
puhrome <phirom.yim@fullsail.edu>
2015-11-05 09:22:03 -0500

    Updated MY_Model
    Added functionality to CRUD method

--- application/core/my_model.php
+++ application/core/my_model.php
@@ -1,22 +1,88 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-class MY_Model extends CI_Controller {
-    const DB_TABLE = 'abstract';
-    const DB_TABLE_PK = 'abstract';
-
-
+class MY_Model extends CI_Model{
+    const DB_TABLE = 'abstract'; //database table
+    const DB_TABLE_PK = 'abstract'; //primary key of db table
 
     /*
-     * create record
+     * Create the 4 CRUD Method
      * */
-    private function insert(){
+
+    /*
+    * Create record
+    * */
+    private function insert() {
         $this->db->insert($this::DB_TABLE, $this);
         $this->{$this::DB_TABLE_PK} = $this->db->insert_id();
     }
 
-    private function update(){
-        $this->db-update($this::DB_TABLE, $this, $this::DB_TABLE_PK);
+    /*
+    * Updated record
+    * */
+    private function update() {
+        $this->db->update($this::DB_TABLE, $this, $this::DB_TABLE_PK);
     }
 
-    public function populate($row)
+    /*
+    * Populate from an array or standard class
+     * $param mixed $row
+    * */
+    public function populate($row) {
+        foreach ($row as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+    /*
+    * Load from Database
+    * $param in $id
+    * */
+    public function load($id) {
+        $query = $this->db->get_where($this::DB_TABLE, array(
+            $this::DB_TABLE_PK => $id,
+        ));
+        $this->populate($query->row());
+    }
+    /*
+    * Delete the current record
+    * */
+    public function delete() {
+        $this->db->delete($this::DB_TABLE, array(
+            $this::DB_TABLE_PK => $this->{$this::DB_TABLE_PK},
+        ));
+        unset($this->{$this::DB_TABLE_PK});
+    }
+    /*
+    * Save the record
+    * */
+    public function save() {
+        if (isset($this->{$this::DB_TABLE_PK})){
+            $this->update();
+        }
+        else {
+            $this->insert();
+        }
+    }
+    /*
+    * Get an array of Models with an optional limit, offset.
+    * @param in $limit optional
+    * @param in $offset Optional, if set, requires $limit
+    * @return array Models populated by DB, keyed by PK
+    * */
+    public function get($limit = 0, $offset = 0) {
+        if ($limit) {
+            $query = $this->db->get($this::DB_TABLE, $limit, $offset);
+        }
+        else {
+            $query = $this->db->get($this::DB_TABLE);
+        }
+        $ret_val = array();
+        $class = get_class($this);
+        foreach ($query->result() as $row) {
+            $model = new $class;
+            $model->populate($row);
+            $ret_val[$row->{$this::DB_TABLE_PK}] = $model;
+        }
+        return $ret_val;
+    }
+
 }
